53e07857c413f012a99a1b5323fe37236d681dae
Chris <abbottci@mail.uc.edu>
2025-06-27 09:40:14 -0400

    Corrections based on feedback from Sufang Lai

--- src/ShipCosts.java
+++ src/ShipCosts.java
@@ -1,20 +1,33 @@
-public class ShipCosts
-{
-    public static void main(String[] args)
-    {
-        final int free_ship = 100;
-        int item_cost = 101;
-        double ship_cost = (item_cost * .02);
-        double paid_ship = (ship_cost + item_cost);
+import java.util.Scanner;
 
-        if (item_cost >= free_ship) {
-            System.out.println("You reached free shipping! $" + item_cost);
-            System.out.println("Your shipping is FREE!");
-        }
-        else
-        {
-            System.out.println("You haven't reached free shipping, yet! $" + item_cost);
-            System.out.println("The cost of shipping is $" + ship_cost);
+public class ShipCosts {
+    public static void main(String[] args) {
+        //Variables
+        Scanner in = new Scanner(System.in);
+        final double free_ship = 100;
+        double item_cost = 0;
+        double ship_cost = 0;
+        double total_cost = 0;
+        String invalid_price = "";
+
+        //Scanner
+        System.out.print("Price of your item: ");
+
+        if (in.hasNextDouble()) {
+            item_cost = in.nextDouble();
+            in.nextLine();
+            //They Spent Enough
+            if (item_cost >= free_ship) {
+                ship_cost = 0;
+                total_cost = item_cost;
+                System.out.println("You reached free shipping!");
+                // They Did not spend enough
+            } else {
+                ship_cost = item_cost * 0.02;
+                total_cost = item_cost + ship_cost;
+                System.out.println("The cost of shipping is $" + ship_cost);
+            }
+            System.out.println("Your total with shipping is: $" + total_cost);
         }
     }
 }
